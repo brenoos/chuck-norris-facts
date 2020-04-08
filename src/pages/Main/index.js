f19e606330ea82fdec8d1ad6f6e1a9a9cf62c092
brenoos <breno.oliver07@gmail.com>
2020-04-07 22:08:11 -0300

    fetching categories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchCategoriesRequest } from '../../store/modules/facts';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCategories } from '../../store/modules/facts';
 
-import { RootContainer, StyledPaper as Paper, LogoRow } from './styles';
+import {
+  RootContainer,
+  StyledPaper as Paper,
+  LogoRow,
+  StyledCircularProgress as CircularProgress,
+} from './styles';
 
 import ChuckNorris from '../../assets/chuck-norris-cartoon.png';
 
 export const Main = () => {
   const dispatch = useDispatch();
 
+  const { categories, loading, error } = useSelector(({ facts }) => ({
+    categories: facts.categories,
+    loading: facts.categoriesLoading,
+    error: facts.categoriesError,
+  }));
+
   useEffect(() => {
-    dispatch(fetchCategoriesRequest());
-  });
+    dispatch(getCategories());
+  }, [dispatch]);
 
   return (
     <RootContainer>
@@ -20,7 +31,20 @@ export const Main = () => {
         <h1>Chuck Norris Facts</h1>
       </LogoRow>
 
-      <Paper />
+      <Paper>
+        <p>Select an category to view a joke</p>
+        {loading && <CircularProgress />}
+
+        {!loading && !error && (
+          <ul>
+            {categories.map((category) => (
+              <li key={category}>{category}</li>
+            ))}
+          </ul>
+        )}
+
+        {error && <p> Unable to retrieve categories</p>}
+      </Paper>
     </RootContainer>
   );
 };
--- src/store/modules/facts/index.js
+++ src/store/modules/facts/index.js
@@ -1,5 +1,6 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
 import produce from 'immer';
+import api from '../../../services/api';
 
 const INITIAL_STATE = {
   categories: [],
@@ -21,7 +22,24 @@ export const fetchCategoriesFailed = createAction(
 export default createReducer(INITIAL_STATE, {
   [fetchCategoriesRequest]: (state) =>
     produce(state, (draft) => {
-      draft.categoriesLoading = true;
       draft.categoriesError = false;
+      draft.categoriesLoading = true;
+    }),
+  [fetchCategoriesSuccess]: (state, { payload }) =>
+    produce(state, (draft) => {
+      draft.categories = payload;
+      draft.categoriesLoading = false;
     }),
 });
+
+export const getCategories = () => {
+  return async (dispatch) => {
+    dispatch(fetchCategoriesRequest());
+    try {
+      const { data } = await api.get('categories');
+      dispatch(fetchCategoriesSuccess(data));
+    } catch (err) {
+      dispatch(fetchCategoriesFailed());
+    }
+  };
+};
