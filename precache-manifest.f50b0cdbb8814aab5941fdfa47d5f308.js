cacc342e48a6106faa1bc144f24f0eb26c641c6c
brenoos <breno.oliver07@gmail.com>
2020-04-08 13:38:12 -0300

    Updates

---
+++ precache-manifest.f50b0cdbb8814aab5941fdfa47d5f308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79e039cc521dc7b9feeaba6bce8fe82",
+    "url": "/chuck-norris-facts/index.html"
+  },
+  {
+    "revision": "167f8d282f61da3b5726",
+    "url": "/chuck-norris-facts/static/js/2.0ab81787.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/chuck-norris-facts/static/js/2.0ab81787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d1dffc03fe5f959263",
+    "url": "/chuck-norris-facts/static/js/main.9a548803.chunk.js"
+  },
+  {
+    "revision": "ce712ef8f74583c379f9",
+    "url": "/chuck-norris-facts/static/js/runtime-main.9c6f3663.js"
+  },
+  {
+    "revision": "b8dd7bdf02f73df4ea94007923bbbcc4",
+    "url": "/chuck-norris-facts/static/media/chuck-norris-cartoon.b8dd7bdf.png"
+  }
+]);
